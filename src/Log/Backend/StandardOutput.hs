da7298c1352c0cc4cf3d68b9de5f621ffca0bac8
Jonathan Jouty <me@jonathanjouty.com>
2016-07-18 13:51:49 +0100

    Commit changes introduced by version 0.5.2
    Git repository was not updated when the package was bumped on Hackage.
    Synced by downloading source from
    https://hackage.haskell.org/package/log-0.5.2

--- src/Log/Backend/StandardOutput.hs
+++ src/Log/Backend/StandardOutput.hs
@@ -1,10 +1,22 @@
-module Log.Backend.StandardOutput (stdoutLogger) where
+module Log.Backend.StandardOutput (
+    simpleStdoutLogger
+  , stdoutLogger
+  ) where
 
 import Prelude
 import qualified Data.Text.IO as T
 
 import Log.Data
+import Log.Internal.Logger
 import Log.Logger
+
+-- | Simple, synchronous logger that prints messages to standard output.
+simpleStdoutLogger :: Logger
+simpleStdoutLogger = Logger {
+    loggerWriteMessage = T.putStrLn . showLogMessage Nothing
+  , loggerWaitForWrite = return ()
+  , loggerFinalizers   = []
+  }
 
 -- | Create logger that prints messages to standard output.
 stdoutLogger :: IO Logger
