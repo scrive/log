4e175fa994a61271c9d2529ed32f358b46c8f7ce
Mikhail Glushenkov <mikhail@scrive.com>
2016-10-04 20:41:55 +0100

    Test suite: use ES refresh API instead of sleeping.
    See #6.

--- test/Test.hs
+++ test/Test.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import Log
 import Log.Backend.ElasticSearch
 
 import Control.Concurrent
+import Control.Monad
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as B8
 import Data.Either (Either(..))
@@ -18,14 +17,30 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Data.Text as T
 
-tests :: ElasticSearchConfig -> Logger -> TestTree
+data TestConfig = TestConfig {
+  testServer :: Server,
+  testIndex  :: IndexName
+  }
+
+mkTestConfig :: ElasticSearchConfig -> IO TestConfig
+mkTestConfig ElasticSearchConfig{..} = do
+  now <- getCurrentTime
+  let testServer  = Server esServer
+      testIndex = IndexName $ T.concat
+                  [ esIndex
+                  , "-"
+                  , T.pack $ formatTime defaultTimeLocale "%F" now
+                  ]
+  return TestConfig{..}
+
+
+tests :: TestConfig -> Logger -> TestTree
 tests config logger = testGroup "Unit Tests" [
   testCase "After logging 'foo', 'foo' is there" $ do
       runLogT "log-test" logger $ do
         logTrace_ "foo"
       waitForLogger logger
-      threadDelay 1000000 -- TODO: get rid of this, waitForLogger
-                          -- should do the job.
+      refreshTestIndex config
       hits <- getNumHits config "foo"
       assertBool "expected at least one hit for 'foo'" (hits > 0),
   testCase "After logging 'foo' thrice, searching 'foo' gives >=3 hits" $ do
@@ -34,7 +49,7 @@ tests config logger = testGroup "Unit Tests" [
         logTrace_ "foo"
         logTrace_ "foo"
       waitForLogger logger
-      threadDelay 1000000
+      refreshTestIndex config
       hits <- getNumHits config "foo"
       assertBool "expected at least 3 hits for 'foo'" (hits >= 3),
   testCase "After logging 'foo' and 'bar', searching 'baz' gives 0 hits" $ do
@@ -42,20 +57,22 @@ tests config logger = testGroup "Unit Tests" [
         logTrace_ "foo"
         logTrace_ "bar"
       waitForLogger logger
+      refreshTestIndex config
       threadDelay 1000000
       hits <- getNumHits config "baz"
       assertBool "expected zero hits for 'baz'"  (hits == 0)
   ]
 
-getNumHits :: ElasticSearchConfig -> Text -> IO Int
-getNumHits ElasticSearchConfig{..} query = do
-  now <- getCurrentTime
-  let testIndex = IndexName $ T.concat
-                  [ esIndex
-                  , "-"
-                  , T.pack $ formatTime defaultTimeLocale "%F" now
-                  ]
-      tquery = TermQuery (Term "message" query) Nothing
+refreshTestIndex :: TestConfig -> IO ()
+refreshTestIndex TestConfig{..} =
+  void . withBH' $ refreshIndex testIndex
+
+  where
+    withBH' = withBH defaultManagerSettings testServer
+
+getNumHits :: TestConfig -> Text -> IO Int
+getNumHits TestConfig{..} query = do
+  let tquery = TermQuery (Term "message" query) Nothing
       search = mkSearch (Just tquery) Nothing
   reply <- withBH' $ searchByIndex testIndex search
   B8.putStrLn (responseBody reply)
@@ -66,8 +83,7 @@ getNumHits ElasticSearchConfig{..} query = do
     Right res -> return . hitsTotal . searchHits $ res
 
   where
-    testServer  = Server esServer
-    withBH'     = withBH defaultManagerSettings testServer
+    withBH' = withBH defaultManagerSettings testServer
 
 main :: IO ()
 main = do
@@ -77,4 +93,5 @@ main = do
         esMapping = "log"
         }
   logger <- elasticSearchLogger config randomIO
-  defaultMain $ tests config logger
+  testConfig <- mkTestConfig config
+  defaultMain $ tests testConfig logger
