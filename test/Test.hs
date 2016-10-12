866d599588d351464be03593718719dca2003cba
Mikhail Glushenkov <mikhail@scrive.com>
2016-10-12 22:32:01 +0100

    Move ES-related bits into its own module.
    Pure refactoring, no functionality change.

--- test/Test.hs
+++ test/Test.hs
@@ -3,35 +3,13 @@ module Main where
 import Log
 import Log.Backend.ElasticSearch
 
-import Data.Aeson
-import Data.Either (Either(..))
-import Data.Text (Text)
-import Data.Time
-import Database.Bloodhound
-import Network.HTTP.Client
 import System.Random
 import Test.Tasty
 import Test.Tasty.HUnit
-import qualified Data.Text as T
 
-data TestConfig = TestConfig {
-  testServer :: Server,
-  testIndex  :: IndexName
-  }
+import Test.ElasticSearch
 
-mkTestConfig :: ElasticSearchConfig -> IO TestConfig
-mkTestConfig ElasticSearchConfig{..} = do
-  now <- getCurrentTime
-  let testServer  = Server esServer
-      testIndex = IndexName $ T.concat
-                  [ esIndex
-                  , "-"
-                  , T.pack $ formatTime defaultTimeLocale "%F" now
-                  ]
-  return TestConfig{..}
-
-
-tests :: TestConfig -> Logger -> TestTree
+tests :: ElasticSearchTestConfig -> Logger -> TestTree
 tests config logger = testGroup "Unit Tests" [
   testCase "After logging 'foo', 'foo' is there" $ do
       runLogT "log-test" logger $ do
@@ -56,27 +34,9 @@ tests config logger = testGroup "Unit Tests" [
       assertBool "expected zero hits for 'baz'"  (hits == 0)
   ]
 
-getNumHits :: TestConfig -> Text -> IO Int
-getNumHits TestConfig{..} query = do
-  let tquery = TermQuery (Term "message" query) Nothing
-      search = mkSearch (Just tquery) Nothing
-  reply <- withBH' $ searchByIndex testIndex search
-  let result = eitherDecode (responseBody reply)
-        :: Either String (SearchResult Value)
-  case result of
-    Left err -> assertString err >> return (-1)
-    Right res -> return . hitsTotal . searchHits $ res
-
-  where
-    withBH' = withBH defaultManagerSettings testServer
-
 main :: IO ()
 main = do
-  let config = ElasticSearchConfig {
-        esServer  = "http://localhost:9200",
-        esIndex   = "logs",
-        esMapping = "log"
-        }
+  let config = defaultElasticSearchConfig
   logger <- elasticSearchLogger config randomIO
-  testConfig <- mkTestConfig config
+  testConfig <- defaultElasticSearchTestConfig config
   defaultMain $ tests testConfig logger
