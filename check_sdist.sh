d24a36e8e349d9a5627066dd110de9d151e1ed3d
Mikhail Glushenkov <mikhail@scrive.com>
2017-03-27 17:07:37 +0100

    Re-generate .travis.yml.

--- check_sdist.sh
+++ check_sdist.sh
@@ -1,11 +1,24 @@
 #! /bin/sh
 
-cabal sdist
-SRC_BASENAME=$(cabal info . | awk '{print $2;exit}')
-SRC_NAME=`echo $SRC_BASENAME | sed 's/-[0-9][\.0-9]*//'`
-tar -C dist/ -xf dist/$SRC_BASENAME.tar.gz
-echo 'packages: ., ../../../log-base, ../../../log-elasticsearch, ../../../log-postgres' > "dist/$SRC_BASENAME/cabal.project"
-cd dist/$SRC_BASENAME/
-cabal new-build --disable-tests --disable-benchmarks $SRC_NAME
-rm -rf ./dist-newstyle
-cabal new-build ${TEST} ${BENCH} $SRC_NAME
+set -euxo pipefail
+
+if [ -f configure.ac ]; then autoreconf -i; fi
+rm -rf dist/
+cabal sdist # test that a source-distribution can be generated
+cd dist/
+SRCTAR=(${PKGNAME}-*.tar.gz)
+SRC_BASENAME="${SRCTAR/%.tar.gz}"
+tar -xvf "./$SRC_BASENAME.tar.gz"
+cd "$SRC_BASENAME/"
+## from here on, CWD is inside the extracted source-tarball
+rm -fv cabal.project.local
+echo 'packages: ., ../../../log-base, ../../../log-elasticsearch, ../../../log-postgres' > "cabal.project"
+# this builds all libraries and executables (without tests/benchmarks)
+rm -f cabal.project.freeze
+cabal new-build -w ${HC} --disable-tests --disable-benchmarks all
+# this builds all libraries and executables (including tests/benchmarks)
+# - rm -rf ./dist-newstyle
+
+# build & run tests
+cabal new-build -w ${HC} ${TEST} ${BENCH} all
+if [ "x$TEST" = "x--enable-tests" ]; then cabal new-test -w ${HC} ${TEST} all; fi
