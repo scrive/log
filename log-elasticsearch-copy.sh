429f4ad0bcc836028345dea3395a70583b1bf535
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-05-03 09:48:04 +0300

    Add copy script

---
+++ log-elasticsearch-copy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -ex
+
+DIR=log-elasticsearch/src/Log/Backend/ElasticSearch/
+FILES="$DIR/V1/Lens.hs $DIR/V1/Internal.hs $DIR/V1.hs"
+
+for SRC in $FILES; do
+    DEST=$(echo $SRC|sed s/V1/V5/)
+
+    cat $SRC | sed s/V1/V5/ > $DEST
+done
+
