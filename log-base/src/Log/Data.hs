4d5e9706ef84a576266fec4ef9cbce9042a005a5
Oleksandr Kylymnychenko <oleksandr@nerdydev.net>
2020-07-18 11:54:24 +0200

    Use json for the entire message
    Make sure we encode everyhing in the JSON line, which will look like
    ```
    {"component":"UDP","data":{"field1": "test"},"insertion_time":" ","message":"","timestamp":"2020-07-18 10:07:27","level":"INFO"}
    
    ```
    
    This will produce the correct jsonlines which can be parsed dirrectly by
    any scripit or ingestion pipeline

--- log-base/src/Log/Data.hs
+++ log-base/src/Log/Data.hs
@@ -70,19 +70,16 @@ data LogMessage = LogMessage {
 showLogMessage :: Maybe UTCTime -- ^ The time that message was added to the log.
                -> LogMessage    -- ^ The actual message.
                -> T.Text
-showLogMessage mInsertionTime LogMessage{..} = T.concat $ [
-    T.pack $ formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" lmTime
-  , case mInsertionTime of
+showLogMessage mInsertionTime LogMessage{..} = textifyData $ object [
+    "timestamp" .= (T.pack $ formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" lmTime)
+  , "insertion_time" .=  (case mInsertionTime of
       Nothing -> " "
-      Just it -> T.pack $ formatTime defaultTimeLocale " (%H:%M:%S) " it
-  , T.toUpper $ showLogLevel lmLevel
-  , " "
-  , T.intercalate "/" $ lmComponent : lmDomain
-  , ": "
-  , lmMessage
-  ] ++ if lmData == emptyObject
-    then []
-    else [" ", textifyData lmData]
+      Just it -> T.pack $ formatTime defaultTimeLocale " (%H:%M:%S) " it)
+  , "level" .= (T.toUpper $ showLogLevel lmLevel)
+  , "component" .=  (T.intercalate "/" $ lmComponent : lmDomain)
+  , "message" .= lmMessage
+  , "data" .= lmData
+  ]
   where
     textifyData :: Value -> T.Text
     textifyData = T.decodeUtf8 . toStrict . encode
