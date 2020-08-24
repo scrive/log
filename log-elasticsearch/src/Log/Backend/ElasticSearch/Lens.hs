3802c7b5af3c46dd7b9fd6c9592c8e65485df928
Andrzej Rybczak <andrzej@rybczak.net>
2020-08-24 02:35:01 +0200

    Mark ES lenses INLINE

--- log-elasticsearch/src/Log/Backend/ElasticSearch/Lens.hs
+++ log-elasticsearch/src/Log/Backend/ElasticSearch/Lens.hs
@@ -21,32 +21,39 @@ type Lens' s a = forall f. Functor f => (a -> f a) -> s -> f s
 
 -- | Elasticsearch server address.
 esServer :: Lens' I.ElasticSearchConfig T.Text
-esServer f esc = fmap (\x -> esc { I.esServer = x }) $ f (I.esServer esc)
+esServer f esc = (\x -> esc { I.esServer = x }) <$> f (I.esServer esc)
+{-# INLINE esServer #-}
 
 -- | Elasticsearch index name.
 esIndex :: Lens' I.ElasticSearchConfig T.Text
-esIndex f esc = fmap (\x -> esc { I.esIndex = x }) $ f (I.esIndex esc)
+esIndex f esc = (\x -> esc { I.esIndex = x }) <$> f (I.esIndex esc)
+{-# INLINE esIndex #-}
 
 -- | Elasticsearch shard count for the named index.
 --
 -- @since 0.10.0.0
 esShardCount :: Lens' I.ElasticSearchConfig Int
-esShardCount f esc = fmap (\x -> esc { I.esShardCount = x }) $ f (I.esShardCount esc)
+esShardCount f esc = (\x -> esc { I.esShardCount = x }) <$> f (I.esShardCount esc)
+{-# INLINE esShardCount #-}
 
 -- | Elasticsearch replica count for the named index.
 --
 -- @since 0.10.0.0
 esReplicaCount :: Lens' I.ElasticSearchConfig Int
-esReplicaCount f esc = fmap (\x -> esc { I.esReplicaCount = x }) $ f (I.esReplicaCount esc)
+esReplicaCount f esc = (\x -> esc { I.esReplicaCount = x }) <$> f (I.esReplicaCount esc)
+{-# INLINE esReplicaCount #-}
 
 -- | Elasticsearch mapping name.
 esMapping :: Lens' I.ElasticSearchConfig T.Text
-esMapping f esc = fmap (\x -> esc { I.esMapping = x }) $ f (I.esMapping esc)
+esMapping f esc = (\x -> esc { I.esMapping = x }) <$> f (I.esMapping esc)
+{-# INLINE esMapping #-}
 
 -- |  Elasticsearch basic authentication username and password.
 esLogin :: Lens' I.ElasticSearchConfig (Maybe (T.Text, T.Text))
-esLogin f esc = fmap (\x -> esc { I.esLogin = x }) $ f (I.esLogin esc)
+esLogin f esc = (\x -> esc { I.esLogin = x }) <$> f (I.esLogin esc)
+{-# INLINE esLogin #-}
 
 -- | Allow basic authentication over non-TLS connections.
 esLoginInsecure :: Lens' I.ElasticSearchConfig Bool
-esLoginInsecure f esc = fmap (\x -> esc { I.esLoginInsecure = x }) $ f (I.esLoginInsecure esc)
+esLoginInsecure f esc = (\x -> esc { I.esLoginInsecure = x }) <$> f (I.esLoginInsecure esc)
+{-# INLINE esLoginInsecure #-}
