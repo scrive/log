54052eda8e6e8d759d95ccacfedfdb03336e1fed
Andrzej Rybczak <arybczak@users.noreply.github.com>
2022-09-20 05:00:40 +0200

    Remove withSimpleStdOutLogger (#82)

--- log-base/src/Log/Backend/StandardOutput.hs
+++ log-base/src/Log/Backend/StandardOutput.hs
@@ -1,7 +1,6 @@
 -- | Stdout logging back-end.
 module Log.Backend.StandardOutput
-  ( withSimpleStdOutLogger
-  , withStdOutLogger
+  ( withStdOutLogger
   , withJsonStdOutLogger
   ) where
 
@@ -14,18 +13,6 @@ import qualified Data.ByteString.Lazy.Char8 as BSL
 import Log.Data
 import Log.Internal.Logger
 import Log.Logger
-
--- | Create a simple, synchronous logger that prints messages to standard output
--- and flushes 'stdout' on each call to 'loggerWriteMessage' for the duration of
--- the given action.
-withSimpleStdOutLogger :: (Logger -> IO r) -> IO r
-withSimpleStdOutLogger = withLogger $ Logger
-  { loggerWriteMessage = \msg -> do
-      T.putStrLn $ showLogMessage Nothing msg
-      hFlush stdout
-  , loggerWaitForWrite = return ()
-  , loggerShutdown     = return ()
-  }
 
 -- | Create a logger that prints messages to standard output for the duration of
 -- the given action.
